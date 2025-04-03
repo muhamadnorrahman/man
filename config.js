715b97b7aa6e17021ae96107f29054f2db6bd2bf
muhamadnorrahman <118180506+muhamadnorrahman@users.noreply.github.com>
2025-04-04 07:24:40 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // Konfigurasi untuk setiap server
+    servers: {
+        // Server 1
+        server1: {
+            name: "PT_LINTAS_INDAH_PURNAMA",
+            botToken: "7552285841:AAETj6hPZmjWQmNgYHuFhNQU14EyIhRQCwg",
+            adminIds: ["7855424312"],
+            customers: {},
+            genieacs: {
+                baseUrl: "http://192.168.8.10:7557",
+                username: "teknisi",
+                password: "internet2511"
+            }
+        },
+        // Tambahkan server lain sesuai kebutuhan
+    }
+}; 
